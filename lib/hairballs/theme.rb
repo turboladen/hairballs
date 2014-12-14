3a3297b2ae61206efc0bea66e9a51698b791041e
Steve Loveless <steve.loveless@gmail.com>
2014-12-14 10:55:58 -0800

    Moved #libraries to Theme

--- lib/hairballs/theme.rb
+++ lib/hairballs/theme.rb
@@ -7,9 +7,51 @@ class Hairballs
 
     attr_accessor :name
 
+    # @param name [Symbol]
     def initialize(name)
       @name = name
       @prompt = Prompt.new
+    end
+
+    # @param libs [Array<String>]
+    def libraries(libs=nil)
+      return @libraries if @libraries && libs.nil? && !block_given?
+
+      if libs.nil? && !block_given?
+        fail ArgumentError, "Must either provide an Array or a block."
+      end
+
+      @libraries = if block_given?
+        libs = yield
+
+        unless libs.kind_of?(Array)
+          fail ArgumentError, "Block must return an Array."
+        end
+
+        libs
+      else
+        libs
+      end
+    end
+
+    # Requires #libraries on load.  If they're not installed, install them.  If
+    # it can't be installed, move on to the next.
+    def require_libraries
+      @libraries.each do |lib|
+        retry_count = 0
+
+        begin
+          next if retry_count == 2
+          puts "Requiring library: #{lib}"
+          require lib
+        rescue LoadError
+          puts "#{lib} not installed; installing now..."
+          Gem.install lib
+          require lib
+          retry_count += 1
+          retry
+        end
+      end
     end
 
     def use!
@@ -22,10 +64,12 @@ class Hairballs
       IRB.conf[:PROMPT_MODE] = irb_name
     end
 
+    # @return [Symbol]
     def irb_name
       @name.to_s.upcase
     end
 
+    # @return [Hairballs::Prompt]
     def prompt
       block_given? ? yield(@prompt) : @prompt
     end
