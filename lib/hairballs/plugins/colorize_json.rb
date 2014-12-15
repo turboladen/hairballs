ce1d4d1d64046f830e96433a5276ce0bf98e5dce
Steve Loveless <steve@agrian.com>
2014-12-15 01:10:28 -0800

    Cleanup and colorization

--- lib/hairballs/plugins/colorize_json.rb
+++ lib/hairballs/plugins/colorize_json.rb
@@ -1,16 +1,20 @@
 require_relative '../../hairballs'
 
+# This *must* get used/loaded after the :wirble plugin to work.
+#
+# TODO: fix to work with using/loading before Wirble.
 Hairballs.add_plugin(:colorize_json) do |plugin|
   plugin.libraries %w[json colorize]
 
   plugin.when_used do
-    class IRB::Irb
+    IRB::Irb.class_eval do
       alias_method :old_output_value, :output_value
 
       def output_value
         is_json = JSON.parse(@context.last_value) rescue nil
 
         if is_json
+          vputs 'return value is JSON-like'
           printf @context.return_format, JSON.pretty_generate(JSON.parse(@context.last_value)).blue
         else
           old_output_value
