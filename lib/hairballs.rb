d309281b468285638495795b744d0961356ff70d
Steve Loveless <steve.loveless@gmail.com>
2014-12-15 01:41:37 -0800

    Moved class methods to eigenclass

--- lib/hairballs.rb
+++ lib/hairballs.rb
@@ -7,48 +7,50 @@ require_relative 'hairballs/plugin'
 class Hairballs
   extend Helpers
 
-  def self.themes
-    @themes ||= []
-  end
+  class << self
+    def themes
+      @themes ||= []
+    end
 
-  def self.add_theme(name)
-    theme = Theme.new(name)
-    yield theme
-    themes << theme
-    vputs "Added theme: #{name}"
+    def add_theme(name)
+      theme = Theme.new(name)
+      yield theme
+      themes << theme
+      vputs "Added theme: #{name}"
 
-    theme
-  end
+      theme
+    end
 
-  def self.use_theme(theme_name)
-    switch_to = themes.find { |theme| theme.name == theme_name }
-    fail "Theme not found: :#{theme_name}." unless switch_to
-    vputs "Using theme: #{theme_name}"
+    def use_theme(theme_name)
+      switch_to = themes.find { |theme| theme.name == theme_name }
+      fail "Theme not found: :#{theme_name}." unless switch_to
+      vputs "Using theme: #{theme_name}"
 
-    switch_to.use!
-  end
+      switch_to.use!
+    end
 
-  # @return [Array<Hairballs::Plugin>]
-  def self.plugins
-    @plugins ||= []
-  end
+    # @return [Array<Hairballs::Plugin>]
+    def plugins
+      @plugins ||= []
+    end
 
-  # @param name [Symbol]
-  def self.add_plugin(name, **options)
-    plugin = Plugin.new(name, options)
-    yield plugin
-    plugins << plugin
-    vputs "Added plugin: #{name}"
+    # @param name [Symbol]
+    def add_plugin(name, **options)
+      plugin = Plugin.new(name, options)
+      yield plugin
+      plugins << plugin
+      vputs "Added plugin: #{name}"
 
-    plugin
-  end
+      plugin
+    end
 
-  # @param plugin_name [Symbol]
-  def self.use_plugin(plugin_name, **options)
-    plugin_to_use = plugins.find { |plugin| plugin.name == plugin_name }
-    fail "Plugin not found: :#{plugin_name}." unless plugin_to_use
-    vputs "Using plugin: #{plugin_name}"
+    # @param plugin_name [Symbol]
+    def use_plugin(plugin_name, **options)
+      plugin_to_use = plugins.find { |plugin| plugin.name == plugin_name }
+      fail "Plugin not found: :#{plugin_name}." unless plugin_to_use
+      vputs "Using plugin: #{plugin_name}"
 
-    plugin_to_use.use!(options)
+      plugin_to_use.use!(options)
+    end
   end
 end
