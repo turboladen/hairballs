1d0c631568b9c8a28b15edb6ec4f29115fd91a16
Steve Loveless <steve@agrian.com>
2014-12-18 16:13:11 -0800

    require_project_lib plugin

---
+++ lib/hairballs/plugins/require_project_lib.rb
@@ -0,0 +1,17 @@
+require_relative '../../hairballs'
+
+# Adds the Object#require_project_lib method as a shortcut for
+#
+#     require 'lib/my_library'          # Or...
+#     require 'lib/my/nested/project'   # i.e. gem = my-nested-project
+#
+Hairballs.add_plugin(:require_project_lib) do |plugin|
+  plugin.on_load do
+    Object.class_eval do
+      def require_project_lib
+        require_dir = File.join(*Hairballs.project_name.split('-'))
+        require_relative "#{Dir.pwd}/lib/#{require_dir}"
+      end
+    end
+  end
+end
